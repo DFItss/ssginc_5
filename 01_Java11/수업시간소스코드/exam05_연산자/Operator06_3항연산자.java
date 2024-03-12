fbf4b2d689862cc2212b6d7119a29f0377d1f746
kyung yeol. in <inky4832@daum.net>
2024-03-12 11:47:44 +0900

    Add files via upload

---
+++ 01_Java11/수업시간소스코드/exam05_연산자/Operator06_3항연산자.java
@@ -0,0 +1,29 @@
+package exam05_연산자;
+
+public class Operator06_3항연산자 {
+
+	public static void main(String[] args) {
+
+		//3. 3항 연산자 		
+		// 문법:   (조건식)?참문장:거짓문장;
+		
+		int n = 100;
+		
+		// 문제1: n 값이 5보다 크면 100을 반환하고 아니면 200 반환하는 코드 작성?
+		
+		System.out.println((n>5)?100:200);
+		
+		int result = (n>5)?100:200;
+		System.out.println(result);
+		
+		
+		// 문제2: n 값이 5보다 크고(and, 그리고)  200보다 작으면 "AAA" 반환하고 아니면 "BBB" 반환하는 코드 작성?
+		
+		System.out.println(((n>5)&&(n<200))?"AAA":"BBB");
+		
+		String result2 = ((n>5)&&(n<200))?"AAA":"BBB";
+		System.out.println(result2);
+		
+		
+	}
+}
