a2dd1f294ca9885bfcc73b308d39f3021a7cb93b
kyung yeol. in <inky4832@daum.net>
2024-03-18 11:18:43 +0900

    Add files via upload

--- 01_Java11/수업시간소스코드/exam09_클래스4_메서드/TestUtil.java
+++ 01_Java11/수업시간소스코드/exam09_클래스4_메서드/TestUtil.java
@@ -11,5 +11,17 @@ public class TestUtil {
 		u.method1(10);
 		u.method1("홍길동");
 		u.method1("홍길동", 10);
+		
+		int result =u.method2();
+		System.out.println(result);
+		
+		String result2 = u.method3();
+		System.out.println(result2);
+		
+		
+		int result3 = u.sum(10, 20);
+		System.out.println(result3);
+		
+		
 	}
 }
