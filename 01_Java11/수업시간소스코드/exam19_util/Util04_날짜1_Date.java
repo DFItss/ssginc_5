58c8f899bdad5ea9f879aad9b24ccb9b408d58f0
kyung yeol. in <inky4832@daum.net>
2024-03-22 15:14:02 +0900

    Add files via upload

---
+++ 01_Java11/수업시간소스코드/exam19_util/Util04_날짜1_Date.java
@@ -0,0 +1,15 @@
+package exam19_util;
+
+import java.util.Date;
+
+public class Util04_날짜1_Date {
+
+	public static void main(String[] args) {
+
+		
+		Date d = new Date();
+		System.out.println(d); 
+		// Fri Mar 22 14:45:08 KST 2024, d.toString() 
+		
+	}
+}
