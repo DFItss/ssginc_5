9bbf52481a774fc89fa79ada34d2df28a1d29791
kyung yeol. in <inky4832@daum.net>
2024-03-25 16:57:49 +0900

    Add files via upload

--- 01_Java11/수업시간소스코드/exam21_generics/GenericText02_사용후.java
+++ 01_Java11/수업시간소스코드/exam21_generics/GenericText02_사용후.java
@@ -16,6 +16,7 @@ class Box2<T>{
 public class GenericText02_사용후 {
 	public static void main(String[] args) {
 		//문자열만 저장
+		Box2<String> x = new  Box2<>();  // new 뒤에 오는 제네릭 타입은 생략 가능하다.
 		Box2<String> b = new  Box2<String>();
 		b.setValue("Hello");
 //b.setValue(100);  // 문제1 해결: 잘못된 데이터를 저장시 컴파일 시점에 알 수 있다.
@@ -24,6 +25,7 @@ public class GenericText02_사용후 {
 		System.out.println(s+"\t" + s.toUpperCase());
 		
 		//날짜만 저장
+		Box2<Date> x2 = new Box2<>();   // new 뒤에 오는 제네릭 타입은 생략 가능하다.
 		Box2<Date> b2 = new Box2<Date>();
 //b2.setValue(100);   // 문제1 해결
 		b2.setValue(new Date());
