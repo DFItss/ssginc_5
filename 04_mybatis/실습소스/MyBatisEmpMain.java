02da9d037b09219732beea62146c30823b27d79c
kyung yeol. in <inky4832@daum.net>
2024-04-05 11:41:20 +0900

    Add files via upload

--- 04_mybatis/실습소스/MyBatisEmpMain.java
+++ 04_mybatis/실습소스/MyBatisEmpMain.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 
 import com.config.MySqlSessionFactory;
@@ -30,14 +32,43 @@ public class MyBatisEmpMain {
 //session.commit();
 //System.out.println(n2 + " 개가 삭제됨.");
 		
-		//3. multi update
-		EmpDTO dto4 = new EmpDTO();
-		dto4.setSal(8500);
-		dto4.setEname("강길동");
-		dto4.setEmpno(3);
-int n3 = session.update("com.config.EmpMapper.update", dto4);
-session.commit();
-System.out.println(n3 + " 개가 수정됨.");
+		//3.선택적 update
+//		EmpDTO dto4 = new EmpDTO();
+//		dto4.setSal(8500);
+//		dto4.setEname("강길동");
+//		dto4.setEmpno(3);
+//int n3 = session.update("com.config.EmpMapper.update", dto4);
+//session.commit();
+//System.out.println(n3 + " 개가 수정됨.");
+		
+		
+		// 4. multi update
+//		List<Integer> list4 = Arrays.asList(3, 7369, 7499);
+//int n4 = session.update("com.config.EmpMapper.updateSal", list4);
+//session.commit();
+//System.out.println(n4 + " 개가 수정됨.");		
+		
+		// 5. 다중 조건
+//		HashMap<String, String> map = new HashMap<>();
+////		map.put("xxx", "MANAGER");
+////		map.put("xxx", "SALESMAN");
+//List<EmpDTO> list5 = session.selectList("com.config.EmpMapper.multiChoose", map);	
+//System.out.println(list5);
+		
+		// 6. 단일 컬럼
+//int cnt = session.selectOne("com.config.EmpMapper.totalCount");
+//System.out.println("총레코드갯수:"+ cnt);
+		
+		//7. 페이징 처리
+		int curPage=1;
+		int perPage=3; // 페이지당 보여줄 레코드 갯수
+		int skip = (curPage-1)*perPage;
+		
+List<EmpDTO> list7 = session.selectList("com.config.EmpMapper.paging", null, 
+		                           new RowBounds(skip, perPage));
+for (EmpDTO xxx : list7) {
+	System.out.println(xxx.getEmpno()+"\t"+xxx.getEname());
+}
 session.close();
 	}
 }
